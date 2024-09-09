9cd66892bedb3193e8f024384ffaeeecb570230f
Duerda <Eduarda.teixeira13@etec.sp.gov.br>
2024-09-09 18:03:12 -0300

    #

---
+++ calculo.js
@@ -0,0 +1,35 @@
+function somar(){
+    var x = 2
+    var y = 2
+    let res = x + y
+
+    document.getElementById("soma").innerHTML=res
+    document.getElementById("conf-soma") .innerHTML="Calculando ✔"
+}
+
+  function subtração() { var x = 2
+    var y = 2
+    let res = x - y
+
+
+    document.getElementById("subtração").innerHTML=res
+    document.getElementById("conf-subtração") .innerHTML="Calculando ✔"
+}
+
+function divisão () { var x = 2
+    var y = 2
+    let res = x / y
+
+
+    document.getElementById("divisão").innerHTML=res
+    document.getElementById("conf-divisão") .innerHTML="Calculando ✔"
+}
+
+function mutiplicação () { var x = 2
+    var y = 2
+    let res = x * y
+
+
+    document.getElementById("mutiplicação").innerHTML=res
+    document.getElementById("conf-mutiplicação") .innerHTML="Calculando ✔"
+}
